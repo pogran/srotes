6770b15de3aada254e5f5ad855a82f228c675ed7
SCAND\bahur <bahur@scand.com>
2017-11-08 16:16:38 +0300

    add env for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,49 @@
 var path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    devtool: 'source-map',
-    entry: [
-        './client/src/index.js'
-    ],
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js',
-        publicPath: '/build/'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js/,
-                loaders: ['babel-loader'],
-                include: path.join(__dirname, 'client/src')
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader'
-                })
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
+module.exports = env => {
+    console.log('env', env);
+
+    return {
+        devtool: 'source-map',
+        entry: [
+            './client/src/index.js'
+        ],
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: 'bundle.js',
+            publicPath: '/build/'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js/,
+                    loaders: ['babel-loader'],
+                    include: path.join(__dirname, 'client/src')
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: 'css-loader'
+                    })
+                },
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    use: [
+                        'file-loader',
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                bypassOnDebug: true,
+                            },
                         },
-                    },
-                ],
-            }
+                    ],
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('styles.css')
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin('styles.css')
-    ]
+    }
 }
