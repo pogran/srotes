467d46af6a7996c00550ed18cccfa483d05ae7aa
SCAND\bahur <bahur@scand.com>
2017-11-10 10:38:17 +0300

    add mock data store

---
+++ server/data/stores.js
@@ -0,0 +1,67 @@
+module.exports = [
+    {
+        id: 1,
+        products: [
+            {
+                id: 1,
+                name: 'apple',
+                count: 3,
+            },
+            {
+                id: 2,
+                name: 'lime',
+                count: 10,
+            },
+            {
+                id: 3,
+                name: 'tomato',
+                count: 3,
+            },
+            {
+                id: 4,
+                name: 'pepper',
+                count: 0,
+            },
+            {
+                id: 5,
+                name: 'celery',
+                count: 3,
+            },
+            {
+                id: 6,
+                name: 'lime2',
+                count: 10,
+            },
+            {
+                id: 7,
+                name: 'bananas',
+                count: 3,
+            },
+            {
+                id: 8,
+                name: 'orange',
+                count: 10,
+            }
+        ]
+    },
+    {
+        id: 2,
+        products: [
+            {
+                id: 1,
+                name: 'orange',
+                count: 4,
+            },
+            {
+                id: 2,
+                name: 'bananas',
+                count: 20,
+            }
+        ]
+    },{
+        id: 3,
+        products: [
+
+        ]
+    }
+]
--- server/routes/stories.js
+++ server/routes/stories.js
@@ -1,40 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let stores = require('../data/stores');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json([
-    {
-      id: 1,
-      products: [
-        {
-          id: 1,
-          name: 'apple',
-          count: 3,
-        },
-        {
-          id: 2,
-          name: 'lime',
-          count: 10,
-        }
-      ]
-    },
-    {
-      id: 2,
-      products: [
-        {
-          id: 1,
-          name: 'orange',
-          count: 4,
-        },
-        {
-          id: 2,
-          name: 'bananas',
-          count: 20,
-        }
-      ]
-    }
-  ]);
+  res.json(stores);
 });
 
 module.exports = router;
