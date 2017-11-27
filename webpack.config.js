05f9eb6cb8cf3ef55861fff157893d7a291b0372
SCAND\bahur <bahur@scand.com>
2017-11-27 11:10:28 +0300

    add scss

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,16 @@ module.exports = env => {
           }]
         },
         {
+          test: /\.scss$/,
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+        },
+        {
           test: /\.css$/,
           use: ExtractTextPlugin.extract({
             fallback: 'style-loader',
