2e94516b31b471cfd3a15e8b6febf90d0d5c7824
SCAND\bahur <bahur@scand.com>
2017-11-28 10:52:43 +0300

    add eslint and fix file rootReducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,10 +1,10 @@
-import {combineReducers} from 'redux';
-import store from './storeReducer';
-import comment from './commentReducer';
+import {combineReducers} from 'redux'
+import store from './storeReducer'
+import comment from './commentReducer'
 
 const rootReducer = combineReducers({
   store,
   comment
-});
+})
 
-export default rootReducer;
+export default rootReducer
